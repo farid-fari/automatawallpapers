801ca6d54055fb2d92ed2be1d08f3b53998b43b2
Farid A <7dare@outlook.com>
2018-08-02 21:34:54 +0200

    improved documentation, controls, randomness

--- README.md
+++ README.md
@@ -2,10 +2,31 @@
 
 A slick wallpaper generator, that works with interesting cellular automata and random initial conditions to generate unique wallpapers.
 
-[Wolfram atlas](http://atlas.wolfram.com/01/01/) for ideas of good Wolfram codes to try.
+Use the [Wolfram atlas](http://atlas.wolfram.com/01/01/) to find interesting Wolfram codes.
 
 ## Examples
 
-![example](examples/one.png?raw=true)
+![example](examples/three.png?raw=true)
 
-![example](examples/two.png?raw=true)
+![example](examples/four.png?raw=true)
+
+## Usage
+
+    ./generate.out 1920 1080 110
+
+The first two digits are the frame width then height of the image to generate.
+
+The third digit is the Wolfram Code for the automata.
+The initial line is random.
+
+Several settings can be altered in `generate.ml` in order to change the way the code is processed, for instance the color palette.
+
+| Key | Action|
+| --- | --- |
+| `+` | Increase code by `1` and regenerate |
+| `-` | Decrease code by `1` and regenerate |
+| `r` | Regenerate with a random code (between 0 and 2³⁰) |
+| `space` | Regenerate with a new initial line |
+| `q` | Exit |
+
+The other keys have no effect.
--- generate.ml
+++ generate.ml
@@ -1,62 +1,88 @@
-(* To be called with resolution as two digits, followed by a Wolfram Code.
-Example: ./generate.out 1920 1080 110 *)
-
 open Graphics;;
 
-Random.init (int_of_float (Unix.gettimeofday ()));;
+Random.self_init ();;
 
+(* Quick exponentiation. *)
 let rec pow = fun i j -> match j with
-  | 0 -> 1
-  | j -> let a = pow i (j/2) in a*a*(if j mod 2 = 1 then i else 1);;
+    | 0 -> 1
+    | j -> let a = pow i (j/2) in a*a*(if j mod 2 = 1 then i else 1);;
 
+(* Initiliazing graphics. *)
 let size = " " ^ Sys.argv.(1) ^ "x" ^ Sys.argv.(2) ^ "+30-50";;
 open_graph size;;
 
 let sx = size_x () and sy = size_y ();;
 
+(* Use this to configure the color palette. *)
 let color_conv = fun t -> match t with
-  | 0 -> rgb 0 255 255
-  | 1 -> rgb 255 0 255
-  | 2 -> rgb 255 255 0
-  | 3 -> rgb 255 255 255
-  | 4 -> rgb 255 0 0
-  | 5 -> rgb 0 255 0
-  | 6 -> rgb 0 0 255
-  | _ -> rgb 0 0 0;;
+    | 0 -> rgb 247 240 82
+    | 1 -> rgb 242 129 35
+    | 2 -> rgb 211 78 36
+    | 3 -> rgb 86 63 27
+    | 4 -> rgb 56 114 108
+    | 5 -> rgb 0 255 0
+    | 6 -> rgb 0 0 255
+    | _ -> rgb 0 0 0;;
 
+(* Recursive loop that draws a color array to a line of the screen. *)
 let rec drawline = fun line n i -> match i with
   | i when i >= sx -> ()
   | i -> set_color (color_conv line.(i)); plot i n; drawline line n (i+1);;
 
-let rec generate_automato = fun colors neighborhood rules previous line ->
+(* Generates a new line from the previous one, then launches draw loop. *)
+let rec generate_automato = fun code colors neighborhood rules previous line ->
     if line = sy || key_pressed () then let s = wait_next_event [Key_pressed] in
-      if s.key = 'a' then generate_automato colors neighborhood rules previous 0 else () else
-    begin
-      let newline = Array.make sx 0 in
-      for j = 0 to sx - 1 do
-        let r = ref 0 in
-        for k = -neighborhood to neighborhood do
-          r := colors * !r + (if j+k < sx && j+k >= 0 then previous.(j+k) else 0)
+      (match s.key with
+        | '+' -> main colors neighborhood (max 0 (code+1))
+        | '-' -> main colors neighborhood (max 0 (code-1))
+        | 'r' -> randomRule colors neighborhood
+        | ' ' -> main colors neighborhood code
+        | 'q' -> exit 0
+        | _ -> generate_automato code colors neighborhood rules previous line (* pretend nothing happened *)
+      )
+    else begin
+        let newline = Array.make sx 0 in
+        for j = 0 to sx - 1 do
+            let r = ref 0 in
+            for k = -neighborhood to neighborhood do
+                r := colors * !r + (if j+k < sx && j+k >= 0 then previous.(j+k) else 0)
+            done;
+            newline.(j) <- rules.(!r)
         done;
-        newline.(j) <- rules.(!r)
-      done;
-      drawline newline line 0;
-      generate_automato colors neighborhood rules newline (line+1)
-    end;;
-
-let rec main = fun colors neighborhood code ->
-  let initial = Array.make sx 0 and
-      rules = pow colors (2*neighborhood+1) in
-  let rule = Array.make rules 0 and curcode = ref code in
-  for j = 0 to rules - 1 do
-    rule.(j) <- !curcode mod colors;
-    curcode := !curcode / colors
-  done;
-  for j = 0 to sx - 1 do
-    initial.(j) <- Random.int colors
-  done;
-  generate_automato colors neighborhood rule initial 0;
-  clear_graph ();
-  main colors neighborhood code;;
-
-main 2 1 (int_of_string Sys.argv.(3));;
+        drawline newline line 0;
+        generate_automato code colors neighborhood rules newline (line+1)
+    end
+and
+(* Takes the number of colors to be used as well as the radius of neighborhoods
+   to be considered (added to the pixel above) and a Wolfram code to begin drawing of an automata.
+   Note that specifying a radius different from 1 or an amount of colors other than 2 will make
+   your code to be considered differently from Wolfram codes. *)
+main = fun colors neighborhood code ->
+    let initial = Array.make sx 0 and
+        rules = pow colors (2*neighborhood+1) in
+    let rule = Array.make rules 0 and curcode = ref (max 0 code) in
+    for j = 0 to rules - 1 do
+        rule.(j) <- !curcode mod colors;
+        curcode := !curcode / colors
+    done;
+    for j = 0 to sx - 1 do
+        initial.(j) <- Random.int colors
+    done;
+    clear_graph ();
+    print_endline ("Format: " ^ Sys.argv.(1) ^ "x" ^ Sys.argv.(2) ^ "; Code: " ^ (string_of_int code));
+    generate_automato code colors neighborhood rule initial 0
+(* Same but with random rules (and NOT a random code). *)
+and randomRule = fun colors neighborhood ->
+    let initial = Array.make sx 0 and
+        rules = pow colors (2*neighborhood+1) in
+    let rule = Array.make rules 0 in
+    for j = 0 to rules - 1 do
+        rule.(j) <- Random.int colors;
+    done;
+    for j = 0 to sx - 1 do
+        initial.(j) <- Random.int colors
+    done;
+    clear_graph ();
+    print_endline ("Format: " ^ Sys.argv.(1) ^ "x" ^ Sys.argv.(2) ^ "; Regle aleatoire");
+    generate_automato 0 colors neighborhood rule initial 0
+in main 2 1 (int_of_string Sys.argv.(3));;
